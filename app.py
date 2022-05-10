c6863c19bbc266fb6ff14e2567078fc0871b641c
sumeet.borkar <sumeet.borkar@clairvoyantsoft.com>
2022-05-10 19:56:49 +0530

    app modularization done with write and read

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-import pandas as pd
 import os
+import sys
+from read import get_file_path, get_reader
+from write import load_db_table
 
 
-# Getting file path dynamically
-def get_file_path(dir, table_name):
-    file_name = os.listdir(f'{dir}\{table_name}')[0]
-    fp = f'{dir}\\{table_name}\\{file_name}'
-    return fp
+def process_table(BASE_DIR, conn, table_name):
+    file_path = get_file_path(BASE_DIR, table_name)
+    json_reader = get_reader(file_path)
+    for df in json_reader:
+        load_db_table(df, conn, table_name, df.columns[0])
+        print(' done')
 
-# Read a data from file
-def get_reader(fp):
-    df = pd.read_json(fp,lines= True,chunksize=1000)
-    return df
+
+def main():
+    BASE_DIR = os.environ.get('BASE_DIR')
+    table_names = sys.argv[1].split(',')
+    configs = dict(os.environ.items())
+    conn = f'postgresql://{configs["DB_USER"]}:{configs["DB_PASS"]}@{configs["DB_HOST"]}:{configs["DB_PORT"]}/{configs["DB_NAME"]}'
+    for table_name in table_names:
+        process_table(BASE_DIR, conn, table_name)
 
 
 if __name__ == '__main__':
-    BASE_DIR = input('BASE_DIR: ')
-    table_name = input('TABLE_NAME: ')
-    fp = get_file_path(BASE_DIR, table_name)
-    print(fp)
-    json_reader = get_reader(fp)
-    for idx, df in enumerate(json_reader):
-        print(f'Number of records in chunk with index {idx} is {df.shape[0]}')
-
+    main()
